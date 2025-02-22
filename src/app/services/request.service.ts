c3294407ab34f1c4d7d9cfd24e0389e90cf88b11
Saul <sauletsanta@hotmail.com>
2025-02-22 23:50:00 +0100

    Creando las interfaces y añadiando al servicio los métodos y variables necesarias para conectarse a la API y extraer la información

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Movies, Response } from '../models/response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequestService {
+export class RequestsService {
 
-  constructor() { }
+  private URL = "https://api.themoviedb.org/3/search/movie?query=";
+  private language = "es-ES";
+  private apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjM3OTE3N2U1NWExNmY3MzFjNzkwZTBlOTBlOGU3MiIsIm5iZiI6MTczOTgwMjgwNC4wNDE5OTk4LCJzdWIiOiI2N2IzNDhiNDVhY2E1YTcxZDI5ZmQ5MGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2rf5WOeJadIJzWxO6QsRoqDxkmDc_RvoMDT5K7cnZOM";
+  private headers = { Authorization: `Bearer ${this.apiKey}` };
+
+  constructor(public http: HttpClient) { }
+
+  //MÉTODO QUE DEVUELVE LAS PELÍCULAS CON EL NOMBRE BUSCADO
+  public getResponse(search: string, page: number): Observable<Movies[]> {
+    return this.http.get<Response>(`${this.URL}${search}&language=${this.language}&page=${page}`, { headers: this.headers }).pipe(
+      map((response: any) => response.results)
+    );
+  }
+  //MÉTODO QUE DEVUELVE EL TOTAL DE PÁGINAS QUE TIENE LA BÚSQUEDA REALIZADA
+  public getPages(search: string, page: number): Observable<number> {
+    return this.http.get<Response>(`${this.URL}${search}&language=${this.language}&page=${page}`, { headers: this.headers }).pipe(
+      map((response: any) => response.total_pages)
+    );
+  }
+
 }
